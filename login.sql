be0b4a490913454c28923cb54f3e40eb1c799645
nithiya1710-sri <165297598+nithiya1710-sri@users.noreply.github.com>
2024-08-24 13:14:17 +0530

    Delete login.sql

--- login.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE IF NOT EXISTS your_database_name;
-USE your_database_name;CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    user_type ENUM('doctor', 'user') NOT NULL
-);
